4a517ca845fcaf177ca6db07c26110d0d1cfc020
Caitlin MacFadyen <caitlinmacfadyen@gmail.com>
2019-02-03 12:50:55 +0000

    slight changes to chapter 7

--- rango/views.py
+++ rango/views.py
@@ -62,7 +62,7 @@ def add_category(request):
 		# Have we been provided with a valid form?
 		if form.is_valid():
 			# save the new category to the database
-			form.save(commit=True)
+			cat = form.save(commit=True)
 			# now that the category is saved 
 			# we could give a confirmation message
 			# but since the most recent category added is on the index page
