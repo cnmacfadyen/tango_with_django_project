51434bc7a4ab65def21700d18f93548f5fa5c16f
Caitlin MacFadyen <caitlinmacfadyen@gmail.com>
2019-02-03 15:08:35 +0000

    Finished chapter 8

--- rango/views.py
+++ rango/views.py
@@ -20,8 +20,11 @@ def index(request):
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-	context_dict = {'boldmessage': "This tutorial has been put together by Caitlin"}
-	return render(request, 'rango/about.html', context=context_dict)
+	# prints out whether the method is a GET or POST
+	print(request.method)
+	# prints out the user name, if no one is logged in it prints 'AnonymousUser'
+	print(request.user)
+	return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
 	# create a context dictionary which we can pass
